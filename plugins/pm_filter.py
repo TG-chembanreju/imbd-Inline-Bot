a0b7fb2645004d8cd8ac01146d8a986bdc2a19b6
TG-PushpaReju <97326699+TG-chembanreju@users.noreply.github.com>
2022-03-13 12:03:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -345,8 +345,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122'),
-                        InlineKeyboardButton('Update Channel', url='https://t.me/subin_works')
+                        InlineKeyboardButton('More Bots', url='https://t.me/pushpa_Reju'),
+                        InlineKeyboardButton('Update Channel', url='https://t.me/updatechannel_forcrime')
                     ]
                     ]
                 
